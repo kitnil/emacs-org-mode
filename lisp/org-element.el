96113787f4cd6f5f0ed892ce9594be38485d71d8
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2015-03-06 11:38:20 +0100

    org-element: Fix `org-element-citation-interpreter'
    * lisp/org-element.el (org-element-citation-interpreter): Add "@" now
      key doesn't contain it anymore.
    
    This fixes 6a9c4476f626aad82b5ee2648c28cb0b517ad913.

--- lisp/org-element.el
+++ lisp/org-element.el
@@ -2824,7 +2824,7 @@ CONTENTS is nil."
   (concat "["
 	  (if (org-element-property :parentheticalp citation) "(cite):" "cite:")
 	  (org-element-interpret-data (org-element-property :prefix citation))
-	  (org-element-property :key citation)
+	  "@" (org-element-property :key citation)
 	  (org-element-interpret-data (org-element-property :suffix citation))
 	  "]"))
 
